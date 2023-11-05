6ecc68487538905882bce3a1dc705e775ac3ac81
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-11-06 01:44:31 +0600

    install mongodb express cors jwt and dotenv and crete index.js and set up file and post mathrd

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.JOB_USER}:${process.env.JOB_PASS}@cluster0.uzdqwnz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jobCollection = client.db("jobSearch").collection("allJobs");
+    const applyJobCollection = client.db("jobSearch").collection("applyJob");
+
+    
+
+    app.post("/addNewJob", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("job search project running");
+});
+
+app.listen(port, () => {
+  console.log(`job search server is running on port: ${port}`);
+});
